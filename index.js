cf77155a8eb3b2c2a8005b70ffb9261100bb2855
Verity Carlos <verity.carlos@gmail.com>
2022-01-30 15:25:48 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+let newArr = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        newArr.push ( `Thank you, ${names[i]}, for the wonderful ${event} gift!` );
+        debugger;
+        }
+    return newArr;
+}
+
+writeCards();
+
+function countDown() {
+    let countDown= 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+        }
+};
